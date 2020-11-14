c056e2895c74e0a49a79afc3e19ef95b700a7946
mochi <benguozakinci@gmail.com>
2020-11-14 13:20:36 +0000

    Add mapbox without input

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MapBoxGL from 'react-map-gl';
 
 const Map = () => {
-    return (
-        <>
+    const [viewport, setViewport] = useState({
+        latitude: 55.6867243,
+        longitude: 12.5700724,
+        width: '100vw',
+        height: '100vh',
+        zoom: 10
+    })
 
-        </>
+    return (
+        <MapBoxGL
+            {...viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPACCESS_TOKEN}
+            onViewportChange={(nextViewport => setViewport(nextViewport))}
+        >
+
+        </MapBoxGL>
     );
 };
 
