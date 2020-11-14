c070bbfa046eecbfaab2833df6c7b77615250f39
mochi <benguozakinci@gmail.com>
2020-11-14 13:28:59 +0000

    Fetch required data

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import MapBoxGL from 'react-map-gl';
 
 const Map = () => {
@@ -8,7 +9,16 @@ const Map = () => {
         width: '100vw',
         height: '100vh',
         zoom: 10
-    })
+    });
+    const [playgrounds, setPlaygrounds] = useState();
+
+    useEffect(() => {
+        axios.get('https://wfs-kbhkort.kk.dk/k101/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=k101:legeplads&outputFormat=json&SRSNAME=EPSG:4326')
+            .then((playgroundData) => {
+                const filteredData = playgroundData.data.features.filter(feature => feature.geometry);
+                setPlaygroundData(filteredData);
+            })
+    }, []);
 
     return (
         <MapBoxGL
