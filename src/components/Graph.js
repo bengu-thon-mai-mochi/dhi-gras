af5558e6856de3d7119a2f53cffb80f4ecda3289
mochi <benguozakinci@gmail.com>
2020-11-15 12:47:54 +0000

    Add functionality to replace 0 to 24 in time

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,12 +2,14 @@ import React, { useState, useContext, useEffect } from 'react';
 import GeoContext from '../geoContext';
 import { AreaChart, Area, Tooltip, XAxis, YAxis, Legend } from 'recharts';
 import axios from 'axios';
+import { Card, CardHeader, CardContent } from '@material-ui/core';
 
 const Graph = () => {
     const {
         locationInfo: {
             latitude,
-            longitude
+            longitude,
+            name
         }
     } = useContext(GeoContext);
     const [chartData, setChartData] = useState();
@@ -19,30 +21,49 @@ const Graph = () => {
 
                 const selectedData = timeseries.map(data => {
                     const date = new Date(data.time);
-                    const hours = date.getHours();
-                    return {
-                        time: hours,
-                        air_temperature: data.data.instant.details.air_temperature
+                    const hour = date.getHours();
+                    if (hour === 0) {
+                        return {
+                            time: 24,
+                            air_temperature: data.data.instant.details.air_temperature
+                        }
+                    } else {
+                        return {
+                            time: hour,
+                            air_temperature: data.data.instant.details.air_temperature
+                        }
                     }
                 });
 
                 setChartData(selectedData);
+                console.log(selectedData)
             })
     }, [])
 
     return (
         <>
-            <AreaChart
-                data={chartData}
-                width={500}
-                height={400}
-            >
-                <XAxis orientation="top" type="number" tickCount={12} dataKey="time" name="time" domain={['dataMin', 'dataMax']} />
-                <YAxis dataKey="air_temperature" name="temperature" unit="C" />
-                <Area type="monotone" dataKey="air_temperature" fill="#8884d8" stroke="#8884d8" lineJointType="monotoneX" />
-                <Tooltip />
-                <Legend />
-            </AreaChart>
+            <Card>
+                <CardHeader title={`Temperature at ${name} next 12 hours`} />
+                <CardContent>
+                    <AreaChart
+                        data={chartData}
+                        width={500}
+                        height={400}
+                    >
+                        <defs>
+                            <linearGradient id="temperatureGradient" x1="0" x2="0" y1="0" y2="1">
+                                <stop offset="20%" stopColor="#ff6000" />
+                                <stop offset="100%" stopColor="#b3b3f2" />
+                            </linearGradient>
+                        </defs>
+                        <XAxis orientation="top" type="number" tickCount={12} dataKey="time" name="time" domain={['dataMin', 'dataMax']} />
+                        <YAxis dataKey="air_temperature" name="Temperature" unit="C" />
+                        <Area dataKey="air_temperature" name="Temperature" type="basis" fill="url(#temperatureGradient)" stroke="#8884d8" />
+                        <Tooltip />
+                        <Legend />
+                    </AreaChart>
+                </CardContent>
+            </Card>
         </>
     );
 };
