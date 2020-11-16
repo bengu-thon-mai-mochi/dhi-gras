ca2bed6079599d710526c6242d08ccaae27ff7e1
mochi <benguozakinci@gmail.com>
2020-11-16 18:24:14 +0000

    Add & style customized tooltip content

---
+++ src/components/CustomTootipContent.js
@@ -0,0 +1,54 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Typography } from '@material-ui/core';
+import Umbrella from '@material-ui/icons/BeachAccess';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        padding: '20px',
+        backgroundColor: 'white'
+    },
+    contentLabel: {
+        fontWeight: 'bold',
+        color: '#b3b3f2'
+    },
+    iconWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    iconRotate: {
+        transform: 'rotate(90deg)',
+        padding: '10'
+    }
+}))
+
+const CustomTooltipContent = ({ active, payload }) => {
+    const classes = useStyles();
+
+    return (
+        (active && payload) &&
+        <Box className={classes.root}
+            border={2}
+            borderColor="secondary.main"
+        >
+            <Umbrella aria-hidden={true} fontSize="large" color="primary" />
+            <Typography variant="h6">{payload[0].payload.time} o'clock </Typography>
+            <Typography>
+                <Box component="span" className={classes.contentLabel}> Temperature: </Box>
+                {payload[0].payload.air_temperature} °C
+                    </Typography>
+            <Typography>
+                <Box component="span" className={classes.contentLabel} color="primary"> Chance of Rain: </Box>
+                {payload[0].payload.probability_of_precipitation} %
+                    </Typography>
+            <Box className={classes.iconWrapper}>
+                <Umbrella fontSize="large" aria-hidden={true} className={classes.iconRotate} color="primary" />
+            </Box>
+        </Box>
+    )
+};
+
+export default CustomTooltipContent;
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -4,6 +4,7 @@ import { AreaChart, Area, Tooltip, XAxis, YAxis, Legend } from 'recharts';
 import axios from 'axios';
 import { Box, Card, CardHeader, CardContent, Fade } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import CustomTooltipContent from './CustomTootipContent';
 
 const useStyles = makeStyles({
     root: {
@@ -28,27 +29,24 @@ const Graph = () => {
             checked
         }
     } = useContext(GeoContext);
+
     const [chartData, setChartData] = useState();
 
     useEffect(() => {
         axios.get(`https://api.met.no/weatherapi/locationforecast/2.0/complete?altitude=0&lat=${latitude}&lon=${longitude}`)
             .then(weatherData => {
                 const timeseries = weatherData.data.properties.timeseries.slice(0, 13);
-
                 const selectedData = timeseries.map(data => {
-                    console.log(data.data.next_1_hours.summary.symbol_code)
 
                     const date = new Date(data.time);
                     const hour = date.getHours();
-
                     return {
                         time: hour,
                         air_temperature: data.data.instant.details.air_temperature,
                         symbol_code: data.data.next_1_hours.summary.symbol_code,
-                        precipitation_amount: data.data.next_1_hours.precipitation_amount
+                        probability_of_precipitation: data.data.next_1_hours.details.probability_of_precipitation
                     }
                 });
-
                 setChartData(selectedData);
             })
     }, [latitude, longitude])
@@ -71,10 +69,10 @@ const Graph = () => {
                                         <stop offset="100%" stopColor="#b3b3f2" />
                                     </linearGradient>
                                 </defs>
-                                <XAxis orientation="top" tickCount={13} fontSize={24} padding={{ left: 20, right: 20 }} tickLine={false} dataKey="time" name="time" domain={['dataMin', 'dataMax']} />
+                                <XAxis orientation="top" tickCount={13} fontSize={24} padding={{ left: 20, right: 20 }} tickLine={false} dataKey="time" name="Time" domain={['dataMin', 'dataMax']} />
                                 <YAxis dataKey="air_temperature" domain={[0, 20]} name="Temperature" unit="C" />
                                 <Area dataKey="air_temperature" name="Temperature" type="basis" fill="url(#temperatureGradient)" stroke="#5ac322" strokeWidth={2} />
-                                <Tooltip />
+                                <Tooltip content={<CustomTooltipContent />} labelFormatter={title => title + " o'clock"} />
                                 <Legend />
                             </AreaChart>
                         </Fade>
