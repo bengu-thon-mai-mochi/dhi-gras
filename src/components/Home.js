212b8f08ddcccd43c76f2661572ee75172ba06ed
mochi <benguozakinci@gmail.com>
2020-11-16 19:58:07 +0000

    Add background image and button to landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,47 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { Box, Typography, Container, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
+import ArrowIcon from '@material-ui/icons/ArrowDropDownCircle';
+import navigationImage from '../assets/undraw_Navigation_re_wxx4.svg';
 
 const useStyles = makeStyles({
     root: {
-        height: '90%',
+        height: '90vh',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingTop: '10',
+        backgroundImage: `url(${navigationImage})`,
+        backgroundSize: 'cover'
     },
     containerStyle: {
-        textAlign: 'center'
+        height: '400px',
+        width: "400px",
+        borderRadius: "50%",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        backgroundColor: '#ffffff80'
     }
 })
 
 
 const Home = () => {
     const classes = useStyles();
+    const history = useHistory();
+
     return (
-        <Box className={classes.root}>
-            <div className={classes.containerStyle}>
-                <Typography variant="h4"> Welcome to <br /> Temperature <br /> at <br />  Playgrounds</Typography>
-            </div>
-        </Box >
+        <Container className={classes.root}>
+            <Box className={classes.containerStyle}>
+                <Typography variant="h3"> Visit <br /> the <br />  Map</Typography>
+                <IconButton color="primary" onClick={() => history.push("/space")} >
+                    <ArrowIcon fontSize="large" />
+                </IconButton>
+            </Box>
+        </Container>
     );
 };
 
